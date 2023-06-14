a8f355c15f4d5229c446e29fd8a51bab41db0ca2
soultan.ali.h <soultan.ali.h@gmail.com>
2023-06-14 14:15:17 +0700

    -

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -57,65 +57,72 @@ const Login = () => {
   return (
     <div className="login d-flex justify-content-center align-items-center">
       <div className="form-container rounded-4">
-        <div className="row">
-          <div className="col px-5 py-4">
-            <img className="mb-5" src={require("../assets/logo.png")} alt="" />
-            <h3>Log in</h3>
-            <p>
-              Selamat Datang kembali! Silahkan isi beberapa detail di bawah ini.
-            </p>
-            <form className="my-4 d-grid gap-2">
-              <div className="d-grid gap-1">
-                <label>Username/ SID</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Masukkan Username atau SID"
-                  onChange={(e) => setUsername(e.target.value)}
-                  onKeyDown={(e) => {
-                    e.key === "Enter" ? handleLogin() : enterKeyHandle();
-                  }}
-                />
-              </div>
-              <div className="d-grid gap-1">
-                <label>Password</label>
-                <div className="d-flex align-items-center">
+        <div className="row gap-0 m-0">
+          <div className="col d-flex justify-content-center align-items-center pt-3 p-0">
+            <div className="login-container">
+              <img
+                className="mb-5"
+                src={require("../assets/logo.png")}
+                alt=""
+              />
+              <h3>Log in</h3>
+              <p>
+                Selamat Datang kembali! Silahkan isi beberapa detail di bawah
+                ini.
+              </p>
+              <form className="my-4 d-grid gap-2">
+                <div className="d-grid gap-1">
+                  <label>Username/ SID</label>
                   <input
-                    type={passwordVisibility === false ? "password" : "text"}
+                    type="text"
                     className="form-control"
-                    placeholder="Masukkan Password"
-                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Masukkan Username atau SID"
+                    onChange={(e) => setUsername(e.target.value)}
                     onKeyDown={(e) => {
                       e.key === "Enter" ? handleLogin() : enterKeyHandle();
                     }}
                   />
-                  <Icon
-                    className="password-visibility"
-                    icon={
-                      passwordVisibility === false
-                        ? "clarity:eye-line"
-                        : "clarity:eye-hide-line"
-                    }
-                    onClick={() => {
-                      setPasswordVisibility(!passwordVisibility);
-                    }}
-                  />
                 </div>
+                <div className="d-grid gap-1">
+                  <label>Password</label>
+                  <div className="d-flex align-items-center">
+                    <input
+                      type={passwordVisibility === false ? "password" : "text"}
+                      className="form-control"
+                      placeholder="Masukkan Password"
+                      onChange={(e) => setPassword(e.target.value)}
+                      onKeyDown={(e) => {
+                        e.key === "Enter" ? handleLogin() : enterKeyHandle();
+                      }}
+                    />
+                    <Icon
+                      className="password-visibility"
+                      icon={
+                        passwordVisibility === false
+                          ? "clarity:eye-line"
+                          : "clarity:eye-hide-line"
+                      }
+                      onClick={() => {
+                        setPasswordVisibility(!passwordVisibility);
+                      }}
+                    />
+                  </div>
+                </div>
+              </form>
+              <div className="d-grid">
+                <button
+                  className="border-0 rounded-2 px-3 py-2"
+                  onClick={handleLogin}
+                >
+                  Masuk
+                </button>
               </div>
-            </form>
-            <div className="d-grid">
-              <button
-                className="border-0 rounded-2 px-3 py-2"
-                onClick={handleLogin}
-              >
-                Masuk
-              </button>
+              <label className="login-message mt-2">
+                {loginMessage !== "" ? "*" + loginMessage : ""}
+              </label>
             </div>
-            <label className="login-message mt-2">
-              {loginMessage !== "" ? "*" + loginMessage : ""}
-            </label>
           </div>
-          <div className="col-6 d-flex justify-content-center align-items-center d-none d-xl-inline">
+          <div className="col-6 d-flex justify-content-center align-items-center d-none d-xl-inline p-0">
             <div
               id="carouselExampleDark"
               className="carousel slide"
